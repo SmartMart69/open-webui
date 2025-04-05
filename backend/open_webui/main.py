833ac983a147173e133f37d04b77acd9fd174256
Martin Gronek <martin@gronek.de>
2025-04-05 21:07:30 +0200

    manifest finally

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -1397,21 +1397,22 @@ async def get_manifest_json():
         "start_url": "/",
         "display": "standalone",
         "background_color": "#000000",
+        "theme_color": "#000000",
         "orientation": "natural",
         "icons": [
             {
-                "src": "/static/logo.png",
+                "src": "/static/web-app-manifest-192x192.png",
+                "sizes": "192x192",
                 "type": "image/png",
-                "sizes": "500x500",
-                "purpose": "any",
+                "purpose": "any"
             },
             {
-                "src": "/static/logo.png",
+                "src": "/static/web-app-manifest-512x512.png",
+                "sizes": "512x512",
                 "type": "image/png",
-                "sizes": "500x500",
-                "purpose": "maskable",
-            },
-        ],
+                "purpose": "maskable"
+            }
+        ]
     }
 
 
