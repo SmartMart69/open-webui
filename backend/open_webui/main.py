8fcb94f68d69824deb018a73aaf163ea3216eb95
Martin Gronek <martin@gronek.de>
2025-05-06 19:58:26 +0200

    manifest background color

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -1490,7 +1490,7 @@ async def get_manifest_json():
             "description": "OmniChat is an open, extensible, user-friendly interface for AI that adapts to your workflow.",
             "start_url": "/",
             "display": "standalone",
-            "background_color": "#343541",
+            "background_color": "#000000",
             "orientation": "any",
             "icons": [
                 {
